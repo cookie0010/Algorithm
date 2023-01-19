a510c9b12782b192832597ba5052ddaf3a741846
cookie0010 <sg.been2017@gmail.com>
2023-01-19 15:22:34 +0900

    7568번 덩치

---
+++ src/Class02/Size2.java
@@ -0,0 +1,31 @@
+package Class02;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Size2 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+        int[][] arr = new int[N][2];
+
+        String[] str;
+
+        for(int i=0; i<N; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = sc.nextInt();
+        }
+
+        for(int i=0; i<N; i++){
+            int cnt = 1;
+
+            for(int j=0;j<N;j++){
+                if(arr[i][0]<arr[j][0]&&arr[i][1]<arr[j][1]) cnt++;
+            }
+            sb.append(cnt).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
