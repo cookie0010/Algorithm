6ff00438974417098bedfbff77f8eff31932cfbf
cookie0010 <sg.been2017@gmail.com>
2023-01-27 12:21:30 +0900

    미완

---
+++ src/Programmers/JoyStick.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JoyStick {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();
+
+    }
+//    public int solution(String name) {
+//        int answer = 0;
+//        return answer;
+//    }
+}
