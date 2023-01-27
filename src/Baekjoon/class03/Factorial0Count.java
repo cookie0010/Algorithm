01221fbfae83fc27d749280bd806f350d6291ffd
cookie0010 <sg.been2017@gmail.com>
2023-01-27 12:08:17 +0900

    1676번 팩토리얼0의 개수

--- src/Baekjoon/class03/Factorial0Count.java
+++ src/Baekjoon/class03/Factorial0Count.java
@@ -1,2 +1,12 @@
-package Baekjoon.class03;public class Factorial0Count {
+package Baekjoon.class03;
+
+import java.util.Scanner;
+
+public class Factorial0Count {
+    static int Five = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N / 5 + N / 25 + N / 125);
+    }
 }
